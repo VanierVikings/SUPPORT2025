72a86521e510ec46161b0a7f79a33569a35c64ce
aarushmehta <aarushmehta29@gmail.com>
2025-04-05 14:10:48 -0400

    imported and added stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,14 +7,26 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.util.sendable.SendableRegistry; 
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
-
+  private final RobotContainer m_robotContainer;     
+  private final Timer m_Timer = new Timer(); 
+  private final PWMSparkMax m_rightDrive = new PWMSparkMax(0);
+  private final PWMSparkMax m_lefttDrive = new PWMSparkMax(1);
+  public final XboxController m_Controller = new XboxController(0);
+  private final DifferentialDrive m_DifferentialDrive = new DifferentialDrive(m_lefttDrive, m_rightDrive); 
+  
   public Robot() {
     m_robotContainer = new RobotContainer();
+    SendableRegistry.addChild(m_DifferentialDrive, m_lefttDrive); 
+    SendableRegistry.addChild(m_DifferentialDrive, m_rightDrive);
   }
 
   @Override
@@ -34,6 +46,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_Timer.restart(); 
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
